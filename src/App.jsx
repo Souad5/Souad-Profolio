9638bb7caee99bd556d0a8c845c32a4e630994de
Souad5 <souadalkabir@gmail.com>
2025-04-30 01:09:32 +0600

    NavBar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import { Outlet } from 'react-router'
 
 function App() {
 
   return (
     <>
-     
+     <Navbar />
+     <Outlet />
     </>
   )
 }
