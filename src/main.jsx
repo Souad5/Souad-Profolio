f66bde7959f6a923a2f85428bc1b7566a8ba7e82
Souad5 <souadalkabir@gmail.com>
2025-08-11 04:04:59 +0600

    skills added

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { Router, RouterProvider } from 'react-router'
+import {  RouterProvider } from 'react-router'
 import router from './Layout/Layouts.jsx'
 
 createRoot(document.getElementById('root')).render(
