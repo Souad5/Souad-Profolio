30a414edf58009e46ee7ecda4daa67f0c5ae5b6a
Souad5 <souadalkabir@gmail.com>
2025-08-11 14:48:56 +0600

    done

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -3,13 +3,25 @@ import { Link } from 'react-router';
 
 const NotFound = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-[url(./404.jpg)] bg-center text-white">
-            <Link to="/">
-                <button className="btn btn-primary">
-                    Go to Home
-                </button>
-            </Link>
+        <>
+        <div className='md:flex md:justify-between md:items-center'>
+            <div className='md:w-1/2 invisible md:visible'>
+                <h1 className="text-4xl font-bold text-center mt-20">404 - Page Not Found</h1>
+                <p className="text-center mt-4">The page you are looking for does not exist.</p>
+                <p className="text-center mt-2">Please check the URL or return to the .</p>
+            </div>
+            <div className='md:w-1/2'>
+                <img
+                    src="/404.jpg"
+                    alt="Not Found"
+                    className="mx-auto md:mt-10 md:w-1/2 "
+                />
+            </div>
         </div>
+            <div className='flex items-center justify-center'>
+                <Link to="/"><button className='btn btn-primary w-[160px] mt-10'>Go to home</button></Link>
+            </div>
+        </>
     );
 };
 
