792a4b11ec2ac7e26cf42671354156063c3394a5
Souad5 <souadalkabir@gmail.com>
2025-08-11 14:55:42 +0600

    Project added

--- src/Data/Projects.js
+++ src/Data/Projects.js
@@ -31,6 +31,17 @@ const projects = [
     description: "A platform for users to share knowledge articles, comment, and like posts.",
     challenges: "Implementing secure JWT auth and optimizing database queries.",
     improvements: "Add notifications and AI-based content suggestions.",
+  },
+  {
+    id: 4,
+    name: "Local Food Waste Management",
+    image: "/project4.png",
+    live: "https://assignment-12-ph-b12.web.app/",
+    github: "https://github.com/Souad5/Local-food-management",
+    techStack: "React, Node.js, Express, MongoDB",
+    description: "A community-driven platform to reduce food waste by connecting restaurants, grocery stores, and individuals with surplus food to those in need.",
+    challenges: "Implementing location-based search and real-time availability updates was challenging.",
+    improvements: "Integrate a mobile app, add food safety verification, and enable automated pickup scheduling.",
   }
 ];
 
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -12,7 +12,7 @@ export default function Projects() {
         <h2 className="text-3xl font-bold mb-10 text-center pb-2">Projects</h2>
 
         {!selectedProject ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
             {projects.map((project, index) => (
               <motion.div
                 key={project.id}
